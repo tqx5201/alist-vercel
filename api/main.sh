23262a121fee24f0b1a03a6162344c0e1e9f546b
zknb-666 <2865467965@qq.com>
2023-01-30 11:39:39 +0800

    Use vercel deploy

--- api/main.sh
+++ api/main.sh
@@ -1,7 +1,13 @@
-# rm -rf alist* #Uncomment this line to update
+import "string@0.0.1"
+import "querystring@1.3.0"
+
+build() {
 if [ ! -f "alist" ];then
   curl -L https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
   tar -zxvf alist.tar.gz
   rm -f alist.tar.gz
 fi
-./alist server --no-prefix
+}
+handler() {
+    ./alist server --no-prefix
+}
