adebf8e04a15e7ff6f25d3493fd28c5059555002
zknb-666 <2865467965@qq.com>
2023-01-30 11:46:45 +0800

    6

--- api/main.sh
+++ api/main.sh
@@ -1,13 +1,9 @@
-import "string@0.0.1"
-import "querystring@1.3.0"
 
-build() {
+handler() {
 if [ ! -f "alist" ];then
   curl -L https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
   tar -zxvf alist.tar.gz
   rm -f alist.tar.gz
 fi
-}
-handler() {
-    ./alist server --no-prefix
+./alist server --no-prefix
 }
