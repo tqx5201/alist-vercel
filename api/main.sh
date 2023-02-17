5258b0ac1464846d1764104cd470274cd34a810d
Im-joker <80298711+zknb-666@users.noreply.github.com>
2023-02-17 16:42:43 +0800

    Update main.sh

--- api/main.sh
+++ api/main.sh
@@ -1,9 +1,7 @@
-build() {
+# rm -rf alist* #Uncomment this line to update
+if [ ! -f "alist" ];then
   curl -L https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
   tar -zxvf alist.tar.gz
   rm -f alist.tar.gz
-}
-
-handler() {
-  ./alist server
-  }
+fi
+./alist start --no-prefix
