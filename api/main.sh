b81053ec0495c6e452e9d3a916625379cbce4ff1
Im-joker <80298711+zknb-666@users.noreply.github.com>
2023-02-17 11:49:01 +0800

    更新 main.sh

--- api/main.sh
+++ api/main.sh
@@ -1,8 +1,5 @@
-if [ ! -f "alist" ];then
+build() {
   curl -L https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
   tar -zxvf alist.tar.gz
   rm -f alist.tar.gz
-fi
-./alist
-
-
+}
