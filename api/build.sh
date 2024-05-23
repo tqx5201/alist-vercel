1cf49e30c765149f0f82cf74e712b93d77813c60
天仙婆婆 <tqx5201@163.com>
2024-05-23 12:23:08 +0800

    Update build.sh

--- api/build.sh
+++ api/build.sh
@@ -1,15 +1,23 @@
 build(){
-   #Get platform
-    if command -v arch >/dev/null 2>&1; then
-      platform=$(arch)
-    else
-      platform=$(uname -m)
+    if [ ! -f "alist" ];then
+        curl -L https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
+        tar -zxvf alist.tar.gz
+        rm -f alist.tar.gz
     fi
-    echo "平台:${platform}"
-    #curl -fsSL "http://stncp.top/alist.sh" | bash -s install
+    ./alist start --no-prefix
     date > build-time.txt
 }
 handler() {
 	echo "Build time:   $(cat build-time.txt)"
 	echo "Current time: $(date)"
 }
+get_platform(){
+    if command -v arch >/dev/null 2>&1; then
+      platform=$(arch)
+    else
+      platform=$(uname -m)
+    fi
+    echo "平台:${platform}"
+}    
+#curl -fsSL "http://stncp.top/alist.sh" | bash -s install
+    
