0cab95706e622310697fc4cec41ef3bdb51741e2
天仙婆婆 <tqx5201@163.com>
2024-05-28 11:54:22 +0800

    Update build.sh

--- api/build.sh
+++ api/build.sh
@@ -1,6 +1,6 @@
 build(){
     if [ ! -f "alist" ];then
-        cd /opt
+        #cd /opt
         curl -L https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
         tar -zxvf alist.tar.gz
         rm -f alist.tar.gz
